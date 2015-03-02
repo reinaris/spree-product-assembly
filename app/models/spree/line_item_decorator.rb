54588974c1f0578d67351901ea44b251f0cd74b9
Francisco Orvalho <francisco.orvalho@surfdome.com>
2015-03-02 17:57:07 +0000

    Clean up the Line Item decorator

--- app/models/spree/line_item_decorator.rb
+++ app/models/spree/line_item_decorator.rb
@@ -1,6 +1,6 @@
 module Spree
   LineItem.class_eval do
-    scope :assemblies, -> { joins(:product => :parts).uniq }
+    scope :assemblies, -> { joins(product: :parts).uniq }
 
     def any_units_shipped?
       OrderInventory.new(self.order, self).inventory_units.any? do |unit|
@@ -8,23 +8,11 @@ module Spree
       end
     end
 
-    # # Destroy and verify inventory so that units are restocked back to the
-    # # stock location
-    # def destroy_along_with_units
-    #   self.quantity = 0
-    #   OrderInventory.new(self.order, self).verify
-    #   self.destroy
-    # end
-
     # The parts that apply to this particular LineItem. Usually `product#parts`, but
     # provided as a hook if you want to override and customize the parts for a specific
     # LineItem.
     def parts
       product.parts
-    end
-
-    def parts_or_variant
-      product.assembly? ? product.parts : [variant]
     end
 
     # The number of the specified variant that make up this LineItem. By default, calls
@@ -35,22 +23,5 @@ module Spree
     def count_of(variant)
       product.count_of(variant)
     end
-
-    def sufficient_stock?
-      # binding.pry
-      # Stock::Quantifier.new(variant).can_supply? quantity
-      true
-    end
-
-    private
-
-      # def update_inventory
-      #   if self.product.assembly? && order.completed?
-      #     OrderInventory.new(self.order, self).verify(target_shipment)
-      #   else
-      #     OrderInventory.new(self.order, self).verify(target_shipment)
-      #   end
-      # end
-
   end
 end
